9d7426beca1582195967b3abec1fe3bea0728744
yaroslavnikiforov <yaroslav.nikiforov.93@gmail.com>
2019-02-24 22:29:05 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+var server = require("http").createServer(app);
+var io = require("socket.io").listen(server);
+
+server.listen(3000);
+
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/index.html");
+});
