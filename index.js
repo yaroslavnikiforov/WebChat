ba410182ef92402996427fa199079ccd73af62dd
yaroslavnikiforov <yaroslav.nikiforov.93@gmail.com>
2019-03-30 18:48:55 +0200

    add bootstrap and create chat layout

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-var express = require("express");
-var app = express();
-var server = require("http").createServer(app);
-var io = require("socket.io").listen(server);
+var express = require('express')
+var app = express()
+var server = require('http').createServer(app)
+var io = require('socket.io').listen(server)
 
-server.listen(3000);
+server.listen(3000)
 
-app.get("/", function(request, response) {
-  response.sendFile(__dirname + "/index.html");
-});
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/index.html')
+})
+
+users = []
+connections = []
