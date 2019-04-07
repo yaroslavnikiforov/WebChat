c244f82dfed3984c64a3fdd6c8f1b2fe7b3dcb62
yaroslavnikiforov <yaroslav.nikiforov.93@gmail.com>
2019-04-07 21:11:33 +0300

    handle chat messages

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-var express = require('express')
-var app = express()
-var server = require('http').createServer(app)
-var io = require('socket.io').listen(server)
+var express = require("express");
+var app = express();
+var server = require("http").createServer(app);
+var io = require("socket.io").listen(server);
 
-server.listen(3000)
+server.listen(3000);
 
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/index.html')
-})
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/index.html");
+});
 
-users = []
-connections = []
+users = [];
+connections = [];
 
-io.sockets.on('connection', socket => {
-  console.log('Connection success')
-  connections.push(socket)
+io.sockets.on("connection", socket => {
+  console.log("Connection success");
+  connections.push(socket);
 
-  socket.on('disconnect', data => {
-    connections.splice(connections.indexOf(socket), 1)
-    console.log('disconnected')
-  })
-})
+  socket.on("disconnect", data => {
+    connections.splice(connections.indexOf(socket), 1);
+    console.log("disconnected");
+  });
+
+  socket.on("send mess", function(data) {
+    io.sockets.emit("add mess", {
+      msg: data.msg,
+      name: data.name,
+      className: data.className
+    });
+  });
+});
